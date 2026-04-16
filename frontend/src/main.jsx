6232dc0ee90b229a7cc0fd7f63e2ee61a795ce53
mdjahidhasan6544-ctrl <mdjahidhasan6544@gmail.com>
2026-04-16 11:05:48 +0600

    fix production route not found

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import "./index.css";
 
+const Router = import.meta.env.PROD ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
